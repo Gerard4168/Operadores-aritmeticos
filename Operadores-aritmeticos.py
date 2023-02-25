d92a636e1dc16eff88bf6b2a014777df2e6c7ea9
Gerard4168 <gerard2234506@correo.uis.edu.co>
2023-02-25 08:24:01 -0500

    Operadores-aritmeticos

---
+++ Operadores-aritmeticos.py
@@ -0,0 +1,31 @@
+# Programa para implementar operadores aritmeticos
+
+print("------------------------------------------")
+print("------------OPERADORES ARITMETICOS--------")
+print("------------------------------------------")
+
+# input
+
+x = int(input("Digite el valor de x: ")) 
+y = int(input("Digite el valor de y: "))
+
+s = x+y
+r = x-y
+m = x*y
+d = x/y
+mod = x%y
+de = x//y
+p = x**y
+
+# output
+print('--------------------------------')
+print('---------Resultados-------------')
+print('--------------------------------')
+print("Suma" + str(s))
+print("Resta" + str(r))
+print("Multiplicacion" + str(m))
+print("Division" + str(d))
+print("Modulo" + str(mod))
+print("Division entero" + str(de))
+print("Potencia" + str(p))
+
